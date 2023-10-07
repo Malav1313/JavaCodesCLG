85d59cc473a02e03b14858d2994c649d7a04de06
Malav1313 <145552831+Malav1313@users.noreply.github.com>
2023-10-07 14:36:28 +0000

    practice

---
+++ distancecalc.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+public class distancecalc{
+    public static void main(String[] args){
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter Your Name : ");
+        String uName = scanner.nextLine();
+        System.out.println("You Entered : " + uName);
+    }
+}
