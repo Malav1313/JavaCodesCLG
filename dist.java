1e6e71ad1cff992ef9760b0dc98027aa4b958b34
Malav1313 <145552831+Malav1313@users.noreply.github.com>
2023-10-09 13:13:02 +0000

    dist prac

---
+++ dist.java
@@ -0,0 +1,40 @@
+import java.util.Scanner;
+
+public class dist{
+    public static void main(String[] args){
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Enter Latitude of 1st point : ");
+        double latitude1 = scanner.nextDouble();
+        
+        System.out.println("Enter longitude of 1st point : ");
+        double longitude1 = scanner.nextDouble();
+
+
+        System.out.println("Enter Latitude of 2nd point : ");
+        double latitude2 = scanner.nextDouble();
+        
+        System.out.println("Enter longitude of 2nd point : ");
+        double longitude2 = scanner.nextDouble();
+
+
+        double dlatitude = latitude2 - latitude1;
+        double dlongitude = longitude2 - longitude1;
+
+
+        double dlatitudeRadian = Math.toRadians(dlatitude);
+        double dlongitudeRadian = Math.toRadians(dlongitude);
+
+
+        double distance = Math.sin(dlatitudeRadian / 2) * Math.sin(dlatitudeRadian / 2) + 
+        Math.cos(latitude1) * Math.cos(latitude2) * Math.sin(dlongitudeRadian / 2) * Math.sin(dlongitudeRadian / 2);
+
+
+        distance = 2 * Math.atan2(Math.sqrt(distance), Math.sqrt(1-distance));
+
+        distance = distance * 6371;
+
+        System.out.println("Distance : " + distance + " Kms");
+    }
+}
